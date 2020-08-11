383e8d2240b81be9fc7ca61ff914014ae1e96ba5
Ana Tekic <atekic@pravobranitelj.local>
2020-08-11 15:23:43 +0200

    added adding colors to user clicked array when user clicks it

--- game.js
+++ game.js
@@ -1,13 +1,13 @@
 
 let gamePattern = [];
 let buttonColours = ["red", "blue", "yellow", "green"];
-
+let userClickedPattern = [];
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
- console.log(randomChosenColour);
+
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
   let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
@@ -16,3 +16,9 @@ function nextSequence() {
 }
 
 $(document).keypress(nextSequence)
+
+$( ".container" ).click(function(event) {
+  let userChosenColour = event.target.id;
+  userClickedPattern.splice(userClickedPattern.length, 0, userChosenColour);
+  console.log(userClickedPattern);
+});
