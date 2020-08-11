0a3e4e5b09a9951aa9c2f2052bb2ea629ef7cf3d
Ana Tekic <atekic@pravobranitelj.local>
2020-08-11 16:04:33 +0200

    added animate press - grey color function to use chosen color

--- game.js
+++ game.js
@@ -3,22 +3,39 @@ let gamePattern = [];
 let buttonColours = ["red", "blue", "yellow", "green"];
 let userClickedPattern = [];
 
+//randomly generate computer sequence with flash and audio
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playsound(randomChosenColour) // dodaj koji zvuk
 
-  let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-
-  audio.play();
 }
 
 $(document).keypress(nextSequence)
 
-$( ".container" ).click(function(event) {
+//log the user clicked button and add it to user array
+$( ".btn" ).click(function(event) {
   let userChosenColour = event.target.id;
   userClickedPattern.splice(userClickedPattern.length, 0, userChosenColour);
   console.log(userClickedPattern);
+  playSound(userChosenColour)
+  animatePress(userChosenColour)
 });
+
+
+//function that plays appropriate sound, to be implemented into other functions
+function playSound (name) {
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+};
+
+//grey out user user Chosen Colour
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed")
+
+setTimeout(function() {
+  $("#"+ currentColour).removeClass('pressed'); }, 500);
+};
