2c7fa126c8cb0d8111885e0077e56296174a72d3
Ana Tekic <atekic@pravobranitelj.local>
2020-08-12 15:44:44 +0200

    game playable, next step managing wrong answer

--- game.js
+++ game.js
@@ -1,41 +1,80 @@
-
 let gamePattern = [];
 let buttonColours = ["red", "blue", "yellow", "green"];
 let userClickedPattern = [];
+let level = 0;
+let started = false;
 
-//randomly generate computer sequence with flash and audio
+//function that plays appropriate sound, to be implemented into other functions
+
+
+
+//randomly generate computer sequence with flash and audio, track and log the level
 function nextSequence() {
+  userClickedPattern = [];
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
+  level++;
+
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-  playsound(randomChosenColour) // dodaj koji zvuk
+  $("#level-title").text("Level " + level);
 
-}
+  playSound(randomChosenColour) // dodaj koji zvuk
 
-$(document).keypress(nextSequence)
+};
 
-//log the user clicked button and add it to user array
-$( ".btn" ).click(function(event) {
+//sound function, implementable for both players
+function playSound(name) {
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+};
+
+//log the user clicked button and add it to user array, run check answer to see if correct
+$(".btn").click(function(event) {
   let userChosenColour = event.target.id;
   userClickedPattern.splice(userClickedPattern.length, 0, userChosenColour);
   console.log(userClickedPattern);
   playSound(userChosenColour)
   animatePress(userChosenColour)
+  checkAnswer(userClickedPattern.length - 1)
+
 });
 
 
-//function that plays appropriate sound, to be implemented into other functions
-function playSound (name) {
-  let audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
-};
 
 //grey out user user Chosen Colour
 function animatePress(currentColour) {
   $("#" + currentColour).addClass("pressed")
 
-setTimeout(function() {
-  $("#"+ currentColour).removeClass('pressed'); }, 500);
+  setTimeout(function() {
+    $("#" + currentColour).removeClass('pressed');
+  }, 100);
+};
+
+// start the game sequence, only the first time a button is clicked
+$(document).keypress(function() {
+  if (!started) {
+    nextSequence();
+    started = true;
+  }
+});
+
+
+function checkAnswer(currentLevel) {
+  //check if current level (last item in the user array is the same as for game)
+  if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
+    console.log("success");
+//if user clicked the samenumber  of times, while successfully guessing the current level, the game will give another color after 1 sec
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        userClickedPattern = []; //reset the number of user clicks
+        nextSequence();
+      }, 1000);
+
+    }
+  } else {
+    console.log("wrong");
+  };
+
 };
